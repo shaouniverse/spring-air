9d220ec8f3c49923de0a8c0ce40ebae7fbfbbdf0
shaoshuai <shaoshuai@yozosoft.com>
2023-08-08 17:10:25 +0800

    自动生成相关三层

--- src/main/java/com/example/demo/tool/ExecuteMybatis.java
+++ src/main/java/com/example/demo/tool/ExecuteMybatis.java
@@ -1,27 +1,30 @@
-//package com.example.demo.tool;
-//
-//import com.baomidou.mybatisplus.annotation.IdType;
-//import com.baomidou.mybatisplus.generator.FastAutoGenerator;
-//
-//public class ExecuteMybatis {
-//    public static void main(String[] args) {
-//        FastAutoGenerator.create("jdbc:mysql://192.168.80.39:3306/dams_server_bak?useUnicode=true&characterEncoding=utf8&nullCatalogMeansCurrent=true&useSSL=false&useLegacyDatetimeCode=false&serverTimezone=GMT%2B8",
-//                "dams_server",
-//                "TR54e$%STs#h")
-//                .globalConfig(builder -> {
-//                    builder.author("shaoshuai")
-//                            .fileOverride()
-//                            .outputDir("C:\\Users\\86183\\Desktop\\dda");
-//                })
-//                .strategyConfig(builder -> {
-//                    builder.addInclude("archive_compile_data_doc")
-//                            .entityBuilder()
-//                            .idType(IdType.ASSIGN_UUID)
-//                            .enableLombok()
-////                            .enableTableFieldAnnotation()//字段属性
-//                            .mapperBuilder()
-//                            .enableMapperAnnotation();
-//                })
-//                .execute();
-//    }
-//}
+package com.example.demo.tool;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.generator.FastAutoGenerator;
+
+public class ExecuteMybatis {
+    public static void main(String[] args) {
+        FastAutoGenerator.create("jdbc:mysql://192.168.80.39:3306/dams_server_bak?useUnicode=true&characterEncoding=utf8&nullCatalogMeansCurrent=true&useSSL=false&useLegacyDatetimeCode=false&serverTimezone=GMT%2B8",
+                "dams_server",
+                "TR54e$%STs#h")
+                .globalConfig(builder -> {
+                    builder.author("shaoshuai")
+                            .outputDir("C:\\Users\\86183\\Desktop\\dda");
+                })
+                .strategyConfig(builder -> {
+                    builder.addInclude("archive_compile_data_doc")
+                            //实体文件覆盖
+                            .entityBuilder()
+                            .idType(IdType.ASSIGN_UUID)
+                            .enableLombok()
+                            //Mapper文件覆盖
+                            .mapperBuilder().enableFileOverride()
+                            //Service文件覆盖
+                            .serviceBuilder().enableFileOverride()
+                            //Controller文件覆盖
+                            .controllerBuilder().enableFileOverride();
+                })
+                .execute();
+    }
+}
