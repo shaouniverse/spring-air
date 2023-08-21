2ca90ae7a4d0cf2e431498eaa5baa79ff3088122
shaoshuai <shaoshuai@yozosoft.com>
2023-08-21 10:43:50 +0800

    针对 .gitignore的重新整理

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -1,11 +1,13 @@
 package com.example.demo.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/demo")
+@Slf4j
 public class DemoController {
 
     @GetMapping("/hello")
