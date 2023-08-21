06ad5dcdf0c51a008f16d4d8814051c8d88bf519
shaoshuai <shaoshuai@yozosoft.com>
2023-08-21 11:51:25 +0800

    test

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
-//@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+//@SpringBootApplication(exclude = DataSourceAutoConfiguration.class )
 @MapperScan(value = "com.example.demo.mapper")
 public class DemoApplication {
 
